4432bc0ab704c301f3190f1a525d74570728c546
Kristi Zentner <kristi.zentner17@gmail.com>
2021-10-12 06:12:21 +0900

    Removed unused imports

--- src/components/Quote.js
+++ src/components/Quote.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { Row, Container, Card, Col } from "react-bootstrap";
 import { IMAGES } from "../store/store";
 import Spinner from "./Spinner";
